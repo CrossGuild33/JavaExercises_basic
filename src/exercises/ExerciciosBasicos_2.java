6edffe07016c13ed05cc52a06713074a8d9fef19
Julio <julio.acc@hotmail.com>
2025-10-22 10:42:10 -0300

    Criando exercício 2: Criação de calculadora

--- src/exercises/ExerciciosBasicos_2.java
+++ src/exercises/ExerciciosBasicos_2.java
@@ -1,7 +1,52 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class ExerciciosBasicos_2 {
     public static void main(String[] args) {
+        // Exercício de criar calculadora
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero:" );
+        double a = scanner.nextDouble();
+        System.out.println("Digite o segundo numero:" );
+        double b = scanner.nextDouble();
+        System.out.println("Escolha um operador(/, *, +, -):");
+        char operator = scanner.next().charAt(0);
+
+
+        double result;
+        switch (operator){
+            case '+':
+                result = a + b;
+                System.out.println("Resultado:" + result);
+                break;
+
+            case '-':
+                result = a - b;
+                System.out.println("Resultado:" + result);
+                break;
+
+            case '/':
+                if (b != 0) {
+                    result = a / b;
+                    System.out.println("Resultado" + result);
+                } else{
+                        System.out.println("Error: não é permitido divisão por zero");
+                    }
+                break;
+
+            case '*':
+                result = a * b;
+                System.out.println("Resultado:" + result);
+                break;
+
+            default:
+                System.out.println("Operador inválido");
+        }
+
+        scanner.close();
 
     }
 }
