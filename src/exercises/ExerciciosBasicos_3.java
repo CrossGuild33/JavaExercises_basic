50db069765bd96a0981b2c18ade69bad99b4a729
Julio <julio.acc@hotmail.com>
2025-10-25 05:31:50 -0300

    feat: Realizando os exercícios de for e for com tabuada

--- src/exercises/ExerciciosBasicos_3.java
+++ src/exercises/ExerciciosBasicos_3.java
@@ -1,9 +1,24 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class ExerciciosBasicos_3 {
     public static void main(String[] args) {
-        int list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+        // exercício de for, para imprimir numeros de 1 a 10.
+       for (int i = 1; i <= 10; i++){
+           System.out.println(i);
+       }
+
+       // exercício For para imprimir numero com a tabuada de 1 a 10
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero:");
+        int num = scanner.nextInt();
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(num * i);
+        }
+
+        scanner.close();
         
     }
 }
