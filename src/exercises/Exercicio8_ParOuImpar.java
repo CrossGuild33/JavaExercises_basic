06d93786d9573ac19da1408a56a559a363250012
Julio <julio.acc@hotmail.com>
2025-10-25 06:17:27 -0300

    feat: Realizando exercício 8

--- src/exercises/Exercicio8_ParOuImpar.java
+++ src/exercises/Exercicio8_ParOuImpar.java
@@ -1,4 +1,19 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class Exercicio8_ParOuImpar {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero:");
+        int num = scanner.nextInt();
+        if(num % 2 == 0){
+            System.out.println("O numero é par");
+        }else {
+            System.out.println("O numero é impar");
+        }
+        }
+
+    }
+
+
